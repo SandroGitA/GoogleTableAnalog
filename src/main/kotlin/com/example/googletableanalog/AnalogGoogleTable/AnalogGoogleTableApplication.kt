604e6600c3f217541d92c2765fe99ebf0a1ac190
SandroGitA <maktavish74@mail.ru>
2023-10-08 17:28:49 +0500

    Initial commit

---
+++ src/main/kotlin/com/example/googletableanalog/AnalogGoogleTable/AnalogGoogleTableApplication.kt
@@ -0,0 +1,11 @@
+package com.example.googletableanalog.AnalogGoogleTable
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnalogGoogleTableApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnalogGoogleTableApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/googletableanalog/AnalogGoogleTable/AnalogGoogleTableApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.googletableanalog.AnalogGoogleTable
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnalogGoogleTableApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
