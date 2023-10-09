8d64e048b561229a651de2318bd8369a47d3e027
SandroGitA <maktavish74@mail.ru>
2023-10-09 21:12:05 +0500

    Add send-data method

--- src/main/kotlin/com/example/googletableanalog/AnalogGoogleTable/Configuration.kt
+++ src/main/kotlin/com/example/googletableanalog/AnalogGoogleTable/Configuration.kt
@@ -2,13 +2,20 @@ package com.example.googletableanalog.AnalogGoogleTable
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
 
 //Класс конфигурации. Описываются методы для взаимодействия с бэкендом
-@Configuration
+@RestController
 class Configuration {
-    
+
     @GetMapping("/")
     fun main(): String {
         return "MainPage"
+    }
+
+    @PostMapping("/send-data")
+    fun sendData(){
+        //
     }
 }
