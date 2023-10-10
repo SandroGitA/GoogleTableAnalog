97f112cbfa08b96c4ec19877833f161350beb45e
SandroGitA <maktavish74@mail.ru>
2023-10-10 22:09:22 +0500

    Add comments in AppLogic

--- src/main/kotlin/com/example/googletableanalog/AnalogGoogleTable/AppLogic.kt
+++ src/main/kotlin/com/example/googletableanalog/AnalogGoogleTable/AppLogic.kt
@@ -1,10 +1,19 @@
 package com.example.googletableanalog.AnalogGoogleTable
 
-//Класс логики бэкенда
-class AppLogic {
+/*Класс логики бэкенда
+Сюда приходят данные, происходит работа
+Предполагается, что данные преобразуются в массив
+Создается аналог таблицы, и парчится каждое значение,
+на предмет операции +-=* / или просто числа
+*/
+class AppLogic(data: String) {
 
     //Буквы столбцов
     val symbolOfColumn: String? = null
+
     //Номера строк
     val numberOfRow: Int = 0
+
+    //Таблица
+    val table = emptyArray<Int>()
 }
