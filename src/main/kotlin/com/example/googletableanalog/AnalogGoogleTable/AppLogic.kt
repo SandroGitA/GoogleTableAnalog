4ef091351b7b36eb223ad08f9072321afe60ba5b
SandroGitA <maktavish74@mail.ru>
2023-10-19 23:23:48 +0500

    Add field to ClientCoordinate class

--- src/main/kotlin/com/example/googletableanalog/AnalogGoogleTable/AppLogic.kt
+++ src/main/kotlin/com/example/googletableanalog/AnalogGoogleTable/AppLogic.kt
@@ -1,19 +1,25 @@
 package com.example.googletableanalog.AnalogGoogleTable
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+
 /*Класс логики бэкенда
 Сюда приходят данные, происходит работа
 Предполагается, что данные преобразуются в массив
-Создается аналог таблицы, и парчится каждое значение,
+Создается аналог таблицы, и парсится каждое значение,
 на предмет операции +-=* / или просто числа
 */
 class AppLogic(data: String) {
+    //Создаем объект для парсинга
+    private val jsonData = jacksonObjectMapper()
 
-    //Буквы столбцов
-    val symbolOfColumn: String? = null
+    val md = jsonData.readValue(data, ClientCoordinate::class.java)
 
-    //Номера строк
-    val numberOfRow: Int = 0
+    val nll = "test"
+}
 
-    //Таблица
-    val table = emptyArray<Int>()
+//Класс координаты,
+//для десериализации из JSON в объект
+class ClientCoordinate() {
+    val coordinate: String = ""
+    val value: String = ""
 }
